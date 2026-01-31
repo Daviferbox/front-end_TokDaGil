d36ee15f42f45fcc5b06f465813a9cd040fe0584
Davi Fernandes <daviferbox@gmail.com>
2026-01-30 22:28:07 -0300

    Header finalizado,porem ainda esta sem interação,no futuro eu implanto isso

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,17 @@
-import "./header.css"
+import "./header.css";
 
 export default function Header() {
-    return(
-        <div className="Fundo">
-            Tok da Gil
-        </div>
-    )
+  return (
+    <header className="teste">
+      <div className="Fundo">
+        <div className="nome">Tok da Gil</div>
+        <nav className="botoes">
+        <span>Menu</span>    
+        <span>Empresa</span>
+        <span>Decorações</span>
+        <span>Contato</span>
+        </nav>
+      </div>
+    </header>
+  );
 }
