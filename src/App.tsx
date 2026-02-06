92ec4ad9b756c5028e7efe6a78bb81410b672f87
Davi Fernandes <daviferbox@gmail.com>
2026-02-05 22:26:08 -0300

    segundo carrosel criado com sucesso,so preciso agora revisar o porque as imagens nao estao carregando

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,18 @@ import './App.css'
 import Home from './pages/home'
 
 function App() {
-  
+
 
   return (
     <div id="app">
-      <div className="routes-container">    
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes> 
+      <div className="routes-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
-   
-    </div>     
-  
+
+    </div>
+
   )
 }
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,6 +11,14 @@ export default function Home() {
     "/images/decoração5.jpg",
   ];
 
+  const images2 = [
+    "/images/chá1.webp",
+    "/images/chá2.webp",
+    "/images/chá3.webp",
+    "/images/chá4.webp",
+    "/images/chá5.webp",
+  ];
+
   const [index, setIndex] = useState(0);
 
   function next() {
@@ -27,24 +35,55 @@ export default function Home() {
     return images[(index + pos + images.length) % images.length];
   }
 
+
+  const [index2, setIndex2] = useState(0);
+
+  function getImage2(pos: number) {
+    return images2[(index2 + pos + images2.length) % images2.length];
+  }
+
+  function next2() {
+    setIndex2((prev) => (prev + 1) % images2.length);
+  }
+
+  function prev2() {
+    setIndex2((prev) =>
+      prev === 0 ? images2.length - 1 : prev - 1
+    );
+  }
+
   return (
+
     <div>
       <Header />
-
       <div className="fundo">
-       
+        <div className="TExtoYYYYYYYYYYYYY">
+          Festa Infantil <br /><br />
+        </div>
         <div className="carousel-3">
           <button className="btn left" onClick={prev}>‹</button>
           <div className="carousel-track">
+            <img src={getImage(-2)} className="img side 2" />
             <img src={getImage(-1)} className="img side" />
             <img src={getImage(0)} className="img center" />
             <img src={getImage(1)} className="img side" />
+            <img src={getImage(2)} className="img side 2" />
           </div>
           <button className="btn right" onClick={next}>›</button>
         </div>
 
-        <div className="">
+        <br /><br /><br />
 
+        <div className="carousel-4 ">
+          <button className="btn left2" onClick={prev2}>‹</button>
+          <div className="carousel-track">
+            <img src={getImage2(-2)} className="img side 2" />
+            <img src={getImage2(-1)} className="img side" />
+            <img src={getImage2(0)} className="img center" />
+            <img src={getImage2(1)} className="img side" />
+            <img src={getImage2(2)} className="img side 2" />
+          </div>
+          <button className="btn right2" onClick={next2}>›</button>
         </div>
       </div>
     </div>
